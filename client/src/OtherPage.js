84954f20343e9258695f37b970cb85ca11f840d6
George Neil <george.neils@gmail.com>
2019-03-22 19:26:57 +0530

    Adding Client Folder

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default () => {
+    return (
+        <div>
+            In Other Page
+            <Link to="/">Go to Home</Link>
+        </div>
+    )
+}
